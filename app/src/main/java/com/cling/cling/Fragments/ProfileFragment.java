cc2b01efb8542d3d026b10b979614228aa5f49dc
tier777 <tier1993@gmail.com>
2014-09-14 01:41:55 +0400

    Styles changes.

--- app/src/main/java/com/cling/cling/Fragments/ProfileFragment.java
+++ app/src/main/java/com/cling/cling/Fragments/ProfileFragment.java
@@ -5,15 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
-import com.cling.cling.Adapters.ActionsAdapter;
 import com.cling.cling.R;
 
 public class ProfileFragment extends Fragment {
-
-    private ListView actionsListView;
-    private ActionsAdapter adapter;
 
     public static ProfileFragment newInstance() {
 
@@ -28,12 +23,6 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
-
-        String[] actions = {"hueta 1", "hueta 2", "hueta 3"};
-
-        adapter = new ActionsAdapter(getActivity(), actions);
-        actionsListView = (ListView) rootView.findViewById(R.id.profileActionsListView);
-        actionsListView.setAdapter(adapter);
 
         return rootView;
     }
