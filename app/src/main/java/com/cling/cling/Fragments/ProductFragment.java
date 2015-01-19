b8d0608605acdc21d92197bc7becf3722474f463
megge <ria6@yandex.ru>
2015-01-19 23:41:38 +0400

    some small fixes for design

--- app/src/main/java/com/cling/cling/Fragments/ProductFragment.java
+++ app/src/main/java/com/cling/cling/Fragments/ProductFragment.java
@@ -2,10 +2,14 @@ package com.cling.cling.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.cling.cling.Adapters.GalleryAdapter;
 import com.cling.cling.R;
@@ -13,6 +17,8 @@ import com.cling.cling.R;
 public class ProductFragment extends Fragment {
 
     private ViewPager galleryPagerView;
+    private Button buyButton;
+
 
     public static ProductFragment newInstance() {
 
@@ -34,6 +40,21 @@ public class ProductFragment extends Fragment {
         galleryPagerView = (ViewPager) rootView.findViewById(R.id.productViewPager);
         galleryPagerView.setAdapter(galleryAdapter);
         galleryAdapter.notifyDataSetChanged();
+
+        buyButton = (Button) rootView.findViewById(R.id.productBuyButton);
+
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.setCustomAnimations(android.R.animator.fade_in,
+                        android.R.animator.fade_out);
+                ft.show(ProfileFragment.newInstance());
+
+                ft.commit();
+//                ProfileFragment.newInstance();
+            }
+        });
 
         return rootView;
     }
