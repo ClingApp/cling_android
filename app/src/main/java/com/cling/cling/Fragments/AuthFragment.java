95756965a7bd9b25794e5f7058724004348a300d
megge <ria6@yandex.ru>
2015-01-20 13:06:35 +0400

    fix auth

--- app/src/main/java/com/cling/cling/Fragments/AuthFragment.java
+++ app/src/main/java/com/cling/cling/Fragments/AuthFragment.java
@@ -1,13 +1,17 @@
 package com.cling.cling.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.cling.cling.MainActivity;
 import com.cling.cling.R;
 
 public class AuthFragment extends Fragment {
@@ -26,31 +30,54 @@ public class AuthFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_auth, container, false);
 
-        Button face = (Button) rootView.findViewById(R.id.facebookButton);
-        Button vk = (Button) rootView.findViewById(R.id.vkontakteButton);
-        Button twitter = (Button) rootView.findViewById(R.id.twitterButton);
+        final Button phoneAuthButton = (Button) rootView.findViewById(R.id.phoneAuthButton);
+        final Button confirmPhoneButton = (Button) rootView.findViewById(R.id.confirmPhoneButton);
 
-        Button registrationButton = (Button) rootView.findViewById(R.id.signUpButton);
-        registrationButton.setOnClickListener(new View.OnClickListener() {
+        final EditText confirmCodeEditText = (EditText) rootView.findViewById(R.id.confirmCodeEditText);
+
+        confirmPhoneButton.setVisibility(View.GONE);
+        confirmCodeEditText.setVisibility(View.GONE);
+
+        phoneAuthButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                /*Intent intent = new Intent(getActivity(), UniversalFragmentActivity.class);
-                intent.putExtra(UniversalFragmentActivity.ARG_FRAGMENT_ID, UniversalFragmentActivity.AppropriateFragments.REGISTRATION.getId());
-                startActivity(intent);
-                getActivity().overridePendingTransition(R.anim.slide_enter, R.anim.slide_leave);*/
-
-                RegistrationDialogFragment.newInstance().show(getFragmentManager(), "registration");
+            public void onClick(View view) {
+                confirmPhoneButton.setVisibility(View.VISIBLE);
+                confirmCodeEditText.setVisibility(View.VISIBLE);
             }
         });
 
-        Button loginButton = (Button) rootView.findViewById(R.id.signInButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        confirmPhoneButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                LoginDialogFragment.newInstance().show(getFragmentManager(), "login");
+            public void onClick(View view) {
+                Log.v("auth", confirmCodeEditText.getText().toString());
+                if ("0000".equals(confirmCodeEditText.getText().toString())) {
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
+
+//        Button registrationButton = (Button) rootView.findViewById(R.id.signUpButton);
+//        registrationButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                /*Intent intent = new Intent(getActivity(), UniversalFragmentActivity.class);
+//                intent.putExtra(UniversalFragmentActivity.ARG_FRAGMENT_ID, UniversalFragmentActivity.AppropriateFragments.REGISTRATION.getId());
+//                startActivity(intent);
+//                getActivity().overridePendingTransition(R.anim.slide_enter, R.anim.slide_leave);*/
+//
+//                RegistrationDialogFragment.newInstance().show(getFragmentManager(), "registration");
+//            }
+//        });
+//
+//        Button loginButton = (Button) rootView.findViewById(R.id.signInButton);
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                LoginDialogFragment.newInstance().show(getFragmentManager(), "login");
+//            }
+//        });
 
         return rootView;
     }
