7e4b3cc5933172b491e662757718aa3ef6aab342
Igor Borodin <igor.borodind@gmail.com>
2015-01-22 16:36:53 +0300

    fix auth fragment

--- app/src/main/java/com/cling/cling/Fragments/AuthFragment.java
+++ app/src/main/java/com/cling/cling/Fragments/AuthFragment.java
@@ -2,21 +2,37 @@ package com.cling.cling.Fragments;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cling.cling.MainActivity;
 import com.cling.cling.R;
 import com.cling.cling.Utilities.PreferencesHelper;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AuthFragment extends Fragment {
+
+    final String regex = "\\(\\d{3}\\)\\d{3}\\-\\d{2}\\-\\d{2}";
+
+    String a;
+    int keyDel;
 
     public static AuthFragment newInstance() {
 
@@ -35,6 +51,7 @@ public class AuthFragment extends Fragment {
         final Button phoneAuthButton = (Button) rootView.findViewById(R.id.phoneAuthButton);
         final Button confirmPhoneButton = (Button) rootView.findViewById(R.id.confirmPhoneButton);
 
+        final EditText phoneAuthEditText = (EditText) rootView.findViewById(R.id.phoneAuthEditText);
         final EditText confirmCodeEditText = (EditText) rootView.findViewById(R.id.confirmCodeEditText);
 
         final TextView errorCodeTextView = (TextView) rootView.findViewById(R.id.errorCodeTextView);
@@ -42,11 +59,48 @@ public class AuthFragment extends Fragment {
         confirmPhoneButton.setVisibility(View.GONE);
         confirmCodeEditText.setVisibility(View.GONE);
 
+        phoneAuthEditText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (phoneAuthEditText.getText().length() == 1) {
+                    phoneAuthEditText.setText(phoneAuthEditText.getText().toString() + "-(");
+                    phoneAuthEditText.setSelection(phoneAuthEditText.getText().length());
+                }
+                else if (phoneAuthEditText.getText().length() == 6) {
+                    phoneAuthEditText.setText(phoneAuthEditText.getText().toString() + ")");
+                    phoneAuthEditText.setSelection(phoneAuthEditText.getText().length());
+                }
+                else if (phoneAuthEditText.getText().length() == 10) {
+                    phoneAuthEditText.setText(phoneAuthEditText.getText().toString() + "-");
+                    phoneAuthEditText.setSelection(phoneAuthEditText.getText().length());
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
         phoneAuthButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                confirmPhoneButton.setVisibility(View.VISIBLE);
-                confirmCodeEditText.setVisibility(View.VISIBLE);
+
+                if (phoneAuthEditText.getText().toString().length() == 11) {
+                    confirmPhoneButton.setVisibility(View.VISIBLE);
+                    confirmCodeEditText.setVisibility(View.VISIBLE);
+                } else {
+                    Toast toast = Toast.makeText(getActivity(), "Error phone", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();                }
             }
         });
 
@@ -62,7 +116,9 @@ public class AuthFragment extends Fragment {
                     Intent intent = new Intent(getActivity(), MainActivity.class);
                     startActivity(intent);
                 } else {
-                    errorCodeTextView.setVisibility(View.VISIBLE);
+                    Toast toast = Toast.makeText(getActivity(), "Error code", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
                 }
             }
         });
